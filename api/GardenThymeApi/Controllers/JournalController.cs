805995a0830b058c80b8fb59d52df27d94bd6c8a
Jared Majewski <jmajewski@campuslabs.com>
2020-04-19 21:01:28 -0400

    Journal controller working

--- api/GardenThymeApi/Controllers/JournalController.cs
+++ api/GardenThymeApi/Controllers/JournalController.cs
@@ -18,15 +18,25 @@ namespace GardenThymeApi.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get(int id)
+        public async Task<IActionResult> Get([FromQuery]int id)
         {
             return Ok((await _queryService.Get<Journal>(TableNames.Journal, new { Id = id })).FirstOrDefault());
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody]Journal userSettings)
+        public async Task<IActionResult> Post([FromBody]Journal journal)
         {
-            return Ok((await _queryService.Upsert<Journal>(TableNames.Journal, new { userSettings.UserId, userSettings.Longitude, userSettings.Latitude })).FirstOrDefault());
+            return Ok((await _queryService.Upsert<Journal>(TableNames.Journal, new
+            {
+                journal.UserId,
+                journal.EntryDateTime,
+                journal.EntryType,
+                journal.PlantId,
+                journal.Quantity,
+                journal.RelatedJournalIds,
+                journal.Longitude,
+                journal.Latitude
+            })).FirstOrDefault());
         }
     }
 }
