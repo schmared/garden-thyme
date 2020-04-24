ca147916e21e24e757599c1096c4cd875ecf4e2a
Jen Gettings <jennifer.wystup@gmail.com>
2020-04-23 20:29:44 -0400

    fix build to not squash the things and pushing up new build to prod

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
