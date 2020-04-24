07b61d1be8feddfac6f650efc03dc3e1ddf18600
Jared Majewski <jmajewski@campuslabs.com>
2020-04-23 21:39:25 -0400

    Added Database project and Fluent Migrator.

---
+++ api/database/Migrations/M0_EmptyDb.cs
@@ -0,0 +1,16 @@
+using FluentMigrator;
+
+namespace database
+{
+    [Migration(0)]
+    public class M0_EmptyDb : Migration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ api/database/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace database
+{
+    internal class Program
+    {
+        // TODO: Finish Implementing Active Database Migrations (Requires FlientMigrator.Runner package)
+        private static void Main(string[] args)
+        {
+        //     using (var scope = CreateServices().CreateScope())
+        //         UpdateDatabase(scope.ServiceProvider);
+        }
+
+        // public static IConfiguration Configuration { get; set; }
+
+        // private static IServiceProvider CreateServices()
+        // {
+        //     Configuration = new ConfigurationBuilder()
+        //         .SetBasePath(Directory.GetCurrentDirectory())
+        //         .AddJsonFile("appsettings.json")
+        //         .Build();
+
+        //     return new ServiceCollection()
+        //         .AddFluentMigratorCore()
+        //         .ConfigureRunner(rb => rb
+        //             .AddSqlServer()
+        //             .WithGlobalConnectionString(Configuration["{ConnectionStrings.database}"])
+        //             .ScanIn(typeof(M0_EmptyDb).Assembly).For.Migrations())
+        //         .AddLogging(lb => lb.AddFluentMigratorConsole())
+        //         .BuildServiceProvider(false);
+        // }
+
+        // private static void UpdateDatabase(IServiceProvider serviceProvider)
+        // {
+        //     serviceProvider.GetRequiredService<IMigrationRunner>().MigrateUp();
+        // }
+    }
+}
